f690bff954461f04988427961db9140389974afc
Ye Jizhou <jonathan@Yes-MacBook-Pro.fios-router.home>
2017-11-26 23:52:45 -0500

    2nd update add update function add and update html file

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page,name='article_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$',views.edit_page,name='edit_page'),
+    url(r'^edit/action$',views.edit_action,name='edit_action'),
+    url(r'^update/(?P<article_id>[0-9]+)$',views.update,name='update_page'),
+    url(r'^update/action/(?P<article_id>[0-9]+)$',views.update_action,name='update_action'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from . import models
 
@@ -8,3 +8,39 @@ from . import models
 def index(request):
     articles = models.Artical.objects.all
     return render(request, 'blog/index.html',{'articles' : articles})
+
+def article_page(request,article_id):
+    article = models.Artical.objects.get(pk = article_id)
+    return render(request,'blog/article_page.html',{'article':article})
+
+def edit_page(request,article_id):
+    if str(article_id) == '0':
+        return render(request,'blog/edit_page.html')
+    article = models.Artical.objects.get(pk = article_id)
+    return render(request,'blog/edit_page.html',{'article':article})
+
+def edit_action(request):
+    title = request.POST.get('Title','TITLE')
+    content = request.POST.get('Content','CONTENT')
+    id = request.POST.get('article_id','null')
+    if(str(id) == 0):
+        models.Artical.objects.create(title=title,content=content)
+        return HttpResponseRedirect('/blog/index')
+    else:
+        article = models.Artical.objects.get(pk = id)
+        article.title = title
+        article.content = content
+        article.save()
+        return render(request,'blog/article_page.html',{'article':article})
+
+
+def update(request,article_id):
+    article = models.Artical.objects.get(pk=article_id)
+    return render(request,'blog/update.html',{'article': article})
+
+def update_action(request,article_id):
+    title = request.POST.get('Title', 'TITLE')
+    content = request.POST.get('Content', 'CONTENT')
+    models.Artical.objects.filter(pk=article_id).update(content = content)
+    models.Artical.objects.filter(pk=article_id).update(title=title)
+    return HttpResponseRedirect('/blog/index')
